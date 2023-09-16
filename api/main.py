b1d33aefaa497a2e4ad9c53df1bc5b535ec37e0d
Nate8888 <wilk.nathan@gmail.com>
2023-09-16 02:33:07 -0400

    Get news route w/ dummies

--- api/main.py
+++ api/main.py
@@ -18,5 +18,24 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def hello_world_api():
     return jsonify({"data": "Hello World"})
 
+@app.route('/news', methods=['GET'])
+@cross_origin()
+def get_news_feed():
+    data = {'news':[
+        {
+            'headline': 'Apple to invest $1 billion in North Carolina campus, create at least 3,000 jobs',
+            'source': 'CNBC',
+            'url': 'https://www.cnbc.com/technology/',
+            'esg_score': 0.8,
+            'esg_points': [
+                'Apple is investing $1 billion in North Carolina as part of a plan to establish a new campus and engineering hub in the Research Triangle area.',
+                'The company said it will create at least 3,000 new jobs in machine learning, artificial intelligence, software engineering and other fields.',
+                'Apple will also establish a $100 million fund to support schools and community initiatives in the greater Raleigh-Durham area and across the state.'
+            ]
+        },
+    ]
+    }
+    return jsonify(data)
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
