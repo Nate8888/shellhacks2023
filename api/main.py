377be98fd8a6e803f970d70b57c5bc228ea9edd3
Nate8888 <wilk.nathan@gmail.com>
2023-09-16 04:51:37 -0400

    Added GCP Storage + ElevenLabs working

--- api/main.py
+++ api/main.py
@@ -7,16 +7,64 @@ import json
 import requests as rq
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from google.cloud import storage
+
 import dotenv
+dotenv.load_dotenv()
 
 app = Flask(__name__)
 
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+storage_client = storage.Client()
+
+# write a function that takes a message and use eleven labs to return an audio file
+def generate_audio(msg):
+    pass
+
+def save_audio_to_gcs(bucket_name, destination_blob_name, stream):
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_file(stream)
+
+def generate_audio():
+    CHUNK_SIZE = 1024
+    url = "https://api.elevenlabs.io/v1/text-to-speech/pNInz6obpgDQGcFmaJgB"
+
+    headers = {
+      "Accept": "audio/mpeg",
+      "Content-Type": "application/json",
+      "xi-api-key": os.getenv('ELEVEN_LABS_KEY')
+    }
+
+    data = {
+      "text": "Hi! My name is Bella, nice to meet you!",
+      "model_id": "eleven_monolingual_v1",
+      "voice_settings": {
+        "stability": 0.5,
+        "similarity_boost": 0.5
+      }
+    }
+
+    response = rq.post(url, json=data, headers=headers)
+
+    # Using 'BytesIO' object to hold the audio content.
+    from io import BytesIO
+    audio_content = BytesIO()
+    
+    for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+        if chunk:
+            audio_content.write(chunk)
+
+    audio_content.seek(0)
+    save_audio_to_gcs('shellhacksbucket', 'output.mp3', audio_content)
+    
+generate_audio()
 
 @app.route('/ok', methods=['POST','GET'])
 @cross_origin()
 def hello_world_api():
+    # generate_audio()
     return jsonify({"data": "Hello World"})
 
 @app.route('/articles', methods=['GET'])
